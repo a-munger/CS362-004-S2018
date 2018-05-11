e5f27fa93793048853632f80ab68abc86fecf467
AMunger <munger.alexander@gmail.com>
2018-05-10 22:27:05 -0500

    add random tests
    add random test code

---
+++ dominion/randomtestcard1.c
@@ -0,0 +1,15 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+	struct gameState state;
+
+	return 0;
+}
