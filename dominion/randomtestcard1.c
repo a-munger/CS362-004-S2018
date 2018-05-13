fe9c75317fb8e91504b3702966ca392d01251ef2
a-munger <mungera@oregonstate.edu>
2018-05-12 23:08:32 -0500

    complete random test creation
    complete random test creation

--- dominion/randomtestcard1.c
+++ dominion/randomtestcard1.c
@@ -7,9 +7,39 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_TESTS 1000
+
+//isGameOver() Random Tests
+
 int main () {
-	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+	int i, s, players, player = 0, handCount, deckCount, seed, card;
 	struct gameState state;
+
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+	
+	srand(time(NULL));
+	
+	printf("---  Random Tests for IS GAME OVER  ---");
+	
+	for(i = 0; i < MAX_TESTS; i++)
+	{
+		seed = rand();
+		players = (rand() % 3) + 2;
+		initializeGame(players, k, seed, &state);
+		
+		deckCount = (rand() % 25) + 1;
+		
+		state.deckCount[player] = deckCount;
+		state.handCount[player] = (rand() % 6) + 1;
+		
+		card = rand() % ARR_SIZE(k);
+		
+		state.supplyCount[card] = 0;
+		
+		s = isGameOver(&state);
+		
+		printf("isGameOver returned: %d\n", s);
+	}
 
 	return 0;
 }
