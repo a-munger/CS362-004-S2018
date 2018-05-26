7a52ae27d248ceb5ba050a25b29a9e506c78071b
AMunger <munger.alexander@gmail.com>
2018-05-26 18:28:22 -0500

    add test functions
    add test functions as outlined by the assignment PDF

---
+++ dominion/unittest1.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "assert.h"
+#include "dominion.h"
+#include "rngs.h"
+#include <time.h>
+
+//whoseTurn()
+
+int main () {
+	struct gameState state;
+
+	int turn = 0;
+	int i, s = 0;
+
+	for (i = 0; i < 15; i++)
+	{
+		state.whoseTurn = turn;
+		s = whoseTurn(&state);
+
+		assert(s == turn);
+
+		turn++;
+	}
+	printf("whoseTurn passed test\n");
+	return 0;
+}
